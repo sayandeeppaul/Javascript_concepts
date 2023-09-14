cf361479871c3b449338425027cd1c58161fb2d3
Sayandeep Paul <104418784+SayandeepPaul@users.noreply.github.com>
2023-09-14 15:58:45 +0000

    comparision operator

---
+++ 07_comparision.js
@@ -0,0 +1,14 @@
+// comparision operator
+
+console.log(2>=2)
+console.log(2<2)
+console.log(2==2)
+console.log(2!=2)
+console.log(2!=='2')
+console.log(2!==2)
+console.log(null<0)
+console.log(null<=0)
+console.log(null==0)
+console.log(null!=0)
+console.log(null>=0)
+console.log(null>0)
