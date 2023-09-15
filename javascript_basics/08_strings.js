f84f1360dce36eb1b75ab543d6a13ef4042ff7a8
Sayandeep Paul <104418784+SayandeepPaul@users.noreply.github.com>
2023-09-15 15:48:39 +0000

    strings in js

---
+++ javascript_basics/08_strings.js
@@ -0,0 +1,27 @@
+let a ="                                 Sayandeep                   "
+let b ="Paul"
+
+// modernize syntazx to display variables
+// console.log(`My name is ${a+" "+b}`);
+
+// methods to declare string using the String class instance=========>
+let newString= new String("Sayandeep")
+// console.log(newString);
+
+// methods of string==============>
+// console.log(a.length);
+// console.log(a.toUpperCase());
+// console.log(a.charAt(2))
+// console.log(a.includes('z'))
+// console.log(a.charCodeAt('S'));
+
+// 0    1   2   3   4   5   6   7   8
+// S    A   Y   A   N   D   E   E   P
+// console.log(a.slice(-4,1));         // when we apply negative index it will start from back
+// console.log(a.indexOf('z'))         // important observation
+// console.log(a.substring(0,4))
+// console.log(a)
+// console.log(a.trim())
+// url=`http://localhost:3000/api/media/64f48906b3dd72468be3-e-700`
+// console.log(url)
+// console.log(url.replace('-e-','-f-'))
