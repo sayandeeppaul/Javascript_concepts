8ead91d34f9ddb6813d9a5e98b08d0f0760f544e
Sayandeep Paul <104418784+SayandeepPaul@users.noreply.github.com>
2023-09-17 17:14:36 +0000

    string type conversion in js

---
+++ 01_javascript_basics/06_stringTypeConversion.js
@@ -0,0 +1,17 @@
+let str1 = "hello"
+let str2 = "sayandeep"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+let a = "1" + 2 + 2
+console.log(a);         //122
+
+let b = 1 + 2 + "2"
+console.log(b);         //32 
+// console.log(typeof b);
+
+// number + string= string
+// if number type goes first js treat the evaluation based on the first data type
+
+console.log(+true)
