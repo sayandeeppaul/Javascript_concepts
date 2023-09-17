24565580bec6f1877bb5ee1e168ea8691fc38e08
Sayandeep Paul <104418784+SayandeepPaul@users.noreply.github.com>
2023-09-17 17:13:26 +0000

    variables in js

---
+++ 01_javascript_basics/02_javascriptVariable.js
@@ -0,0 +1,16 @@
+//make the variable fixed
+const accountId=1234
+
+//used in industries in current days
+let accountEmail="sayandeep775@gmail.com"
+
+//used in older version
+var password="12345abc"
+
+//not recommended
+accountState=12334
+
+console.log(accountState)
+
+
+
