b700847d8f43bdfbca7ec82512bc60457817a05f
Sayandeep Paul <104418784+SayandeepPaul@users.noreply.github.com>
2023-09-17 17:16:52 +0000

    Number and math functions

---
+++ 01_javascript_basics/09_NumberAndMathfunction.js
@@ -0,0 +1,11 @@
+// console.log((Math.random()*10))
+// console.log(Math.round((Math.random()*10)))
+
+let minRange=1000
+let maxrange=9999
+
+let avgRange=maxrange-minRange
+
+console.log(Math.round(((Math.random()*avgRange)+minRange)))
+
+// 0.54321 4888.43+1000=5888.433
