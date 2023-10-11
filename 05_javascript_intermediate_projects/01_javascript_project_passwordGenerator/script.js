4448488565e81ecfee21beeddc57b467e3001f6c
Sayandeep Paul <104418784+SayandeepPaul@users.noreply.github.com>
2023-10-11 16:01:18 +0000

    password generator

--- 05_javascript_intermediate_projects/01_javascript_project_passwordGenerator/script.js
+++ 05_javascript_intermediate_projects/01_javascript_project_passwordGenerator/script.js
@@ -1,43 +1,80 @@
-let password= document.querySelector("#password")
-let message= document.querySelector("#message")
-let strength= document.querySelector("#strength")
-let eyeIcon= document.getElementById("eye-icon")
+let password = document.querySelector("#password")
+let message = document.querySelector("#message")
+let strength = document.querySelector("#strength")
+let eyeIcon = document.getElementById("eye-icon")
+let generateBtn = document.querySelector(".generate-btn")
 
-password.addEventListener("input",()=>{
-    if (password.value.length>0) {
-        message.style.display='block'
+let passwordLength = 12
+
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+let numbers = "0123456789"
+let symbols = "!@#$%^&*(){}[]?><:;+-/"
+
+// 4            5             4           5
+// ABCD         abcde       1234        !@#$%
+
+//passwordLength    password.length
+//    12         >       6
+//    12                 18
+
+//12 > 10
+
+const allChars = upperCase + lowerCase + numbers + symbols
+
+function generatePassword() {
+    let randomPassword = ""
+    randomPassword += upperCase[Math.floor(Math.random() * upperCase.length)]
+    randomPassword += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    randomPassword += numbers[Math.floor(Math.random() * numbers.length)]
+    randomPassword += symbols[Math.floor(Math.random() * symbols.length)]
+
+    while (passwordLength > randomPassword.length) {
+        randomPassword += allChars[Math.floor(Math.random() * allChars.length)]
     }
-    else{
-        message.style.display='none'
+    password.value = randomPassword
+}
+
+function copyPassword() {
+    password.select()
+    document.execCommand("copy")
+}
+
+password.addEventListener("input", () => {
+    if (password.value.length > 0) {
+        message.style.display = 'block'
     }
-    if(password.value.length<4){
-        strength.innerText="Weak"
-        strength.style.color="red" 
-        password.style.border="2px solid red"
-        if (password.value.length===0) {
-            password.style.border="none"
+    else {
+        message.style.display = 'none'
+    }
+    if (password.value.length < 4) {
+        strength.innerText = "Weak"
+        strength.style.color = "red"
+        password.style.border = "2px solid red"
+        if (password.value.length === 0) {
+            password.style.border = "none"
         }
 
     }
-    else if(password.value.length>=4 && password.value.length<8){
-        strength.innerText="Medium"
-        strength.style.color="orange"
-        password.style.border="2px solid orange"
+    else if (password.value.length >= 4 && password.value.length < 8) {
+        strength.innerText = "Medium"
+        strength.style.color = "orange"
+        password.style.border = "2px solid orange"
     }
-    else{
-        strength.innerText="Strong"
-        strength.style.color="green"
-        password.style.border="2px solid green"
+    else {
+        strength.innerText = "Strong"
+        strength.style.color = "green"
+        password.style.border = "2px solid green"
     }
 })
 
-eyeIcon.onclick = ()=>{
-    if(password.type=='password'){
-        password.type='text'
-        eyeIcon.innerText="visibility"
+eyeIcon.onclick = () => {
+    if (password.type == 'password') {
+        password.type = 'text'
+        eyeIcon.innerText = "visibility"
     }
-    else{
-        password.type='password'
-        eyeIcon.innerText="visibility_off"
+    else {
+        password.type = 'password'
+        eyeIcon.innerText = "visibility_off"
     }
 }
